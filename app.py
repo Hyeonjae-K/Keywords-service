f8f236a3ca1c241b03264117e161c1417e861ccc
unknown <rnjsguswo012@hufs.ac.kr>
2021-02-18 16:34:31 +0900

    함수 자동 실행 기능 추가

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import time, atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template
 app = Flask(__name__)
+
+
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
 
 
 @app.route('/')
 def hello():
     return render_template("index.html")
 
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=3)
+scheduler.start()
 
 if __name__ == '__main__':
     app.run()
