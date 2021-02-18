d73bbffb68ae76423286c0e8cf2902e057af7173
unknown <rnjsguswo012@hufs.ac.kr>
2021-02-19 00:43:29 +0900

    인자 값 변수명 변경

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-sites = {}
-data = {}
+sites_dic = {}
+data_dic = {}
 
 
 def set_sites():
@@ -14,16 +14,16 @@ def set_sites():
             title, space, url = f.readline().strip().rpartition(" ")
             if title == '':
                 break
-            sites[title] = url
+            sites_dic[title] = url
 
 
 def set_data():
-    data.clear()
+    data_dic.clear()
     date = time.strftime("%Y%m%d")
 
-    for site in sites:
+    for site in sites_dic:
         path = "./test-data/data/{site}/{date}.txt".format(site=site, date=date)
-        data[site] = {}
+        data_dic[site] = {}
 
         with open(path, "r", encoding="utf-8") as f:
             while True:
@@ -39,15 +39,13 @@ def set_data():
                         titles.append(title)
                         urls.append(url)
 
-                    data[site].update({rec_time: [titles, urls]})
+                    data_dic[site].update({rec_time: [titles, urls]})
 
 
 @app.route('/')
 def index():
     set_data()
-    print(data)
-    print(sites)
-    return render_template("index.html", data=data, sites=sites)
+    return render_template("index.html", data=data_dic, sites=sites_dic)
 
 
 # scheduler = BackgroundScheduler()
