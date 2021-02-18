67e7b0e70b19f72dcb1fa42fb9ebf4f2be957544
unknown <rnjsguswo012@hufs.ac.kr>
2021-02-18 23:46:43 +0900

    Heroku 오류 찾기 전체 주석 및 예제 코드 추가

--- app.py
+++ app.py
@@ -1,54 +1,65 @@
-import time
-# from apscheduler.schedulers.background import BackgroundScheduler
-from flask import Flask, render_template
+# import time
+# # from apscheduler.schedulers.background import BackgroundScheduler
+# from flask import Flask, render_template
+#
+#
+# app = Flask(__name__)
+# sites = {}
+# dic = {}
+#
+# # with open('./test-data/data/sites.txt', 'r', encoding='utf-8') as f:
+# #     while True:
+# #         title, space, url = f.readline().strip().rpartition(" ")
+# #         if title == '':
+# #             break
+# #         sites[title] = url
+#
+#
+# def set_data():
+#     dic.clear()
+#     date = time.strftime("%Y%m%d")
+#
+#     for site in sites:
+#         path = "./test-data/data/{site}/{date}.txt".format(site=site, date=date)
+#         dic[site] = {}
+#
+#         with open(path, "r", encoding="utf-8") as f:
+#             while True:
+#                 rec_time = f.readline().strip()
+#                 titles = []
+#                 urls = []
+#
+#                 if rec_time == '':
+#                     break
+#                 else:
+#                     for i in range(10):
+#                         title, space, url = f.readline().strip().rpartition(" ")
+#                         titles.append(title)
+#                         urls.append(url)
+#
+#                     dic[site].update({rec_time: [titles, urls]})
+#
+#
+# @app.route('/')
+# def index():
+#     # set_data()
+#     return render_template("index.html")
+#
+#
+# # scheduler = BackgroundScheduler()
+# # scheduler.add_job(func=set_data, trigger="interval", minutes=1)
+# # scheduler.start()
+#
+# if __name__ == '__main__':
+#     app.run()
 
-
+from flask import Flask
 app = Flask(__name__)
-sites = {}
-dic = {}
-
-with open('./test-data/data/sites.txt', 'r', encoding='utf-8') as f:
-    while True:
-        title, space, url = f.readline().strip().rpartition(" ")
-        if title == '':
-            break
-        sites[title] = url
-
-
-def set_data():
-    dic.clear()
-    date = time.strftime("%Y%m%d")
-
-    for site in sites:
-        path = "./test-data/data/{site}/{date}.txt".format(site=site, date=date)
-        dic[site] = {}
-
-        with open(path, "r", encoding="utf-8") as f:
-            while True:
-                rec_time = f.readline().strip()
-                titles = []
-                urls = []
-
-                if rec_time == '':
-                    break
-                else:
-                    for i in range(10):
-                        title, space, url = f.readline().strip().rpartition(" ")
-                        titles.append(title)
-                        urls.append(url)
-
-                    dic[site].update({rec_time: [titles, urls]})
-
 
 @app.route('/')
 def index():
-    # set_data()
-    return render_template("index.html")
+    return "<h1>Hello World</h1>"
 
-
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(func=set_data, trigger="interval", minutes=1)
-# scheduler.start()
 
 if __name__ == '__main__':
     app.run()
