9aaf17755fe789fa5425386cc0cb39f53185baeb
unknown <rnjsguswo012@hufs.ac.kr>
2021-02-19 11:36:46 +0900

    데이터 저장 방식 변경

--- app.py
+++ app.py
@@ -4,50 +4,37 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-sites_dic = {}
 data_dic = {}
 
 
-def set_sites():
+def set_data():
     with open('./test-data/data/sites.txt', 'r', encoding='utf-8') as f:
         while True:
-            title, space, url = f.readline().strip().rpartition(" ")
-            if title == '':
+            site, space, url = f.readline().strip().rpartition(" ")
+            if site == '':
                 break
-            sites_dic[title] = url
-
-
-def set_data():
-    data_dic.clear()
+            data_dic[site] = {"url": url}
     date = time.strftime("%Y%m%d")
-
-    for site in sites_dic:
-        path = "./test-data/data/{site}/{date}.txt".format(site=site, date=date)
-        data_dic[site] = {}
-
+    for site in data_dic:
+        path = './test-data/data/{site}/{date}.txt'.format(site=site, date=date)
         with open(path, "r", encoding="utf-8") as f:
             while True:
-                rec_time = f.readline().strip()
+                data_time = f.readline().strip()
                 titles = []
                 urls = []
-
-                if rec_time == '':
+                if data_time == '':
                     break
-                else:
-                    for i in range(10):
-                        title, space, url = f.readline().strip().rpartition(" ")
-                        titles.append(title)
-                        urls.append(url)
-
-                    data_dic[site].update({rec_time: [titles, urls]})
+                for i in range(10):
+                    title, space, url = f.readline().strip().rpartition(" ")
+                    titles.append(title)
+                    urls.append(url)
+                data_dic[site][data_time] = {"titles": titles, "urls": urls}
 
 
 @app.route('/')
 def index():
     set_data()
-    print(sites_dic)
-    print(data_dic)
-    return render_template("test.html", data=data_dic)
+    return render_template("test.html", data=data_dic, name="HJ")
 
 
 # scheduler = BackgroundScheduler()
@@ -55,5 +42,4 @@ def index():
 # scheduler.start()
 
 if __name__ == '__main__':
-    set_sites()
     app.run()
