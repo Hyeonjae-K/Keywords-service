95c2549d0410eb708dedaafb676c686af140b5cf
unknown <rnjsguswo012@hufs.ac.kr>
2021-02-19 13:30:11 +0900

    인자값 전달 확인

--- app.py
+++ app.py
@@ -1,46 +1,82 @@
-import time
+# import time
+# from flask import Flask, render_template
+#
+#
+# app = Flask(__name__)
+# sites_dic = {}
+# data_dic = {}
+#
+#
+# def set_sites():
+#     with open('./test-data/data/sites.txt', 'r', encoding='utf-8') as f:
+#         while True:
+#             site, space, url = f.readline().strip().rpartition(" ")
+#             if site == '':
+#                 break
+#             sites_dic[site] = {"url": url}
+#
+#
+# def set_data():
+#     date = time.strftime("%Y%m%d")
+#     for site in sites_dic:
+#         path = f'./test-data/data/{site}/{date}.txt'.format(site=site, date=date)
+#         data_dic[site] = {}
+#         with open(path, "r", encoding="utf-8") as f:
+#             while True:
+#                 data_time = f.readline().strip()
+#                 titles = []
+#                 urls = []
+#                 if data_time == '':
+#                     break
+#                 for i in range(10):
+#                     title, space, url = f.readline().strip().rpartition(" ")
+#                     titles.append(title)
+#                     urls.append(url)
+#                 data_dic[site][data_time] = [titles, urls]
+#
+#
+# @app.route('/')
+# def index():
+#     set_data()
+#     return render_template("test.html", sites=sites_dic)
+#
+#
+# if __name__ == '__main__':
+#     set_sites()
+#     app.run()
+
+
 from flask import Flask, render_template
-
-
+import time
 app = Flask(__name__)
-sites_dic = {}
-data_dic = {}
+sites = ["Google", "Nate", "Zum", "DreamWiz", "KOREA.COM"]
+data = {}
 
 
-def set_sites():
-    with open('./test-data/data/sites.txt', 'r', encoding='utf-8') as f:
-        while True:
-            site, space, url = f.readline().strip().rpartition(" ")
-            if site == '':
-                break
-            sites_dic[site] = {"url": url}
-
-
-def set_data():
+def get_data():
     date = time.strftime("%Y%m%d")
-    for site in sites_dic:
-        path = f'./test-data/data/{site}/{date}.txt'.format(site=site, date=date)
-        data_dic[site] = {}
-        with open(path, "r", encoding="utf-8") as f:
+    for site in sites:
+        data[site] = {}
+        path = f"./test-data/data/{site}/{date}.txt".format(site=site, date=date)
+        with open(path, "r", encoding="UTF-8") as f:
             while True:
                 data_time = f.readline().strip()
+                if data_time == "":
+                    break
                 titles = []
                 urls = []
-                if data_time == '':
-                    break
                 for i in range(10):
                     title, space, url = f.readline().strip().rpartition(" ")
                     titles.append(title)
                     urls.append(url)
-                data_dic[site][data_time] = [titles, urls]
-
+                data[site].update({data_time: [titles, urls]})
+    print(data)
 
 @app.route('/')
 def index():
-    set_data()
-    return render_template("test.html", sites=data_dic)
+    get_data()
+    return render_template("test.html", data=data)
 
 
 if __name__ == '__main__':
-    set_sites()
     app.run()
